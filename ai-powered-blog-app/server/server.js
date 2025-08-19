f1e3e7c5e6482a11a48942724269b5b58135cd22
divyavaddam <divyavaddam@gmail.com>
2025-08-19 08:37:49 +0530

    Updated server.js

--- ai-powered-blog-app/server/server.js
+++ ai-powered-blog-app/server/server.js
@@ -6,6 +6,7 @@ import adminRouter from "./routes/adminRoutes.js";
 import blogRouter from "./routes/blogRoutes.js";
 
 const app = express();
+const server = http.createServer(app);
 
 await connectDB();
 
@@ -18,10 +19,12 @@ app.get("/", (req, res) => res.send("API is working"));
 app.use("/api/admin", adminRouter);
 app.use("/api/blog", blogRouter);
 
-const PORT = process.env.PORT || 3000;
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 3000;
+  server.listen(PORT, () => {
+    console.log("Server is running on port " + PORT);
+  });
+}
 
-app.listen(PORT, () => {
-  console.log("Server is running on port " + PORT);
-});
-
-export default app;
+// EXPORT SERVER FOR VERCEL
+export default server;
