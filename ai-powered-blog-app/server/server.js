88b79b047115472f76237a09b4770d588d9a8a01
divyavaddam <divyavaddam@gmail.com>
2025-08-19 14:03:10 +0530

    Changed server.js

--- ai-powered-blog-app/server/server.js
+++ ai-powered-blog-app/server/server.js
@@ -1,23 +1,15 @@
 import express from "express";
 import "dotenv/config";
-import http from "http";
 import cors from "cors";
 import connectDB from "./configs/db.js";
 import adminRouter from "./routes/adminRoutes.js";
 import blogRouter from "./routes/blogRoutes.js";
-import { HttpResponse } from "@google/genai";
 
 const app = express();
-const server = http.createServer(app);
 
-(async () => {
-  try {
-    await connectDB();
-    console.log("MongoDB Connected");
-  } catch (err) {
-    console.error("DB Connection Failed:", err.message);
-  }
-})();
+connectDB().catch((err) => {
+  console.error("Failed to connect to database:", err);
+});
 
 // Middlewares
 app.use(cors());
@@ -28,12 +20,6 @@ app.get("/", (req, res) => res.send("API is working"));
 app.use("/api/admin", adminRouter);
 app.use("/api/blog", blogRouter);
 
-// Local Development Mode
-if (process.env.NODE_ENV !== "production") {
-  const PORT = process.env.PORT || 3000;
-  server.listen(PORT, () => {
-    console.log("Server running on port " + PORT);
-  });
-}
 
-export default server;
+
+export default app;
